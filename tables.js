ef0fa40e64b42e2c6c358c6700f62212710e7d7c
Nick Neuman <neecholaus@gmail.com>
2017-10-19 23:24:06 -0700

    Tables-6 Made build and injection functions more modular and ready for more table types.

--- tables.js
+++ tables.js
@@ -5,45 +5,76 @@ class Table {
     this.options = data.options;
     this.data = data;
     this.type = data.options.type ? data.options.type : 'vertical_bar';
+
     
-    this.buildItems = this.runBuildFunction();
-    
-    this.injectTable();
+    this.runBuildFunction();   
   }
 
 
 
   runBuildFunction() {
-    var items = [];
-    // will run a different build function depending on type of table user 
-    switch (this.type) {
-	
-      case 'vertical_bar':
-	for(var item in this.data.items) {
-	  var height = this.data.max - (this.data.max / this.data.items[item].height);
-	  var item = `<div class="vertical-bar" data-height="${height}"><div class="vertical-bar-title">${this.data.items[item].name}</div></div>`;
-	  items.push(item);
-	}
-	break;
-    }
-    // assign array of items to class
-    return items;
+    // Stores built HTML strings and final injection function
+    this.buildInjection();
+    // Runs final injection
+    this.runInjection();
   }
 
-  
-  // Puts data into container
-  injectTable() {
-    var con = document.getElementById(this.container);
-    var htmlString = '';
-    for(var item in this.buildItems) {
-      htmlString += this.buildItems[item];
+
+
+  // Loops through data and builds html string
+  // based on type of table
+  buildInjection() {
+    switch (this.type) {
+      case 'vertical_bar':
+	this.buildItems = this.vertical_bar_build();
+	this.injection = this.vertical_bar_injection();
+	break;
     }
-    con.innerHTML = htmlString;
+  }
+
+
+  // Runs the final injection function
+  runInjection() {
+    this.injection();
+  }
+  
+  
+
+
+  
+  
+  // Vertical Bar Table Builds
+  // HTML String builder
+  vertical_bar_build() {
+    var items = [];
+    for(var item in this.data.items) {
+      var height = this.data.max - (this.data.max / this.data.items[item].height);
+      var item = `<div class="vertical-bar" data-height="${height}"><div class="vertical-bar-title">${this.data.items[item].name}</div></div>`;
+      items.push(item);
+    }
+    return items;
+  }
+  // Returns the final injection method
+  vertical_bar_injection() {
+    return function() {
+      var con = document.getElementById(this.container);
+      var htmlString = '';
+      for(var item in this.buildItems) {
+	htmlString += this.buildItems[item];
+      }
+      con.innerHTML = htmlString;
+    }
   }
 
 }
 
 
+
+
+
+
+
+// Test Data for a vertical bar table.
 var data = {
   'items' : [
     {'name': 'bar1', 'height': 7},
