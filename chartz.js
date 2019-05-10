f9e485463636dccc9f42997abfcf06f1b43924b2
neecholaus <thisisnickadam@gmail.com>
2019-05-09 20:07:09 -0700

    Fixed x axis showing undefined for bars that omitted the value.

--- chartz.js
+++ chartz.js
@@ -28,37 +28,37 @@ class Chart {
         }
     }
 
-	/*
-	 * Returns {bars, axes}
-	 * Axes is optional, but bars is required.
-	 */
+    /*
+     * Returns {bars, axes}
+     * Axes is optional, but bars is required.
+     */
     build() {
         let bars = [],
-			axes = [];
+        axes = [];
 
         for (let idx in this.data.items) {
             let item = this.data.items[idx],
-                classes = item.classes ? item.classes.join(' ') : '';
+            classes = item.classes ? item.classes.join(' ') : '';
 
             let a =  '<div class="chart-item-container">',
                 b = `<div class="chart-item ${classes}" data-height="${item.height}">`,
-				c = '';
+                c = '';
 
-			if(item.name) {
+            if(item.name) {
                 c = `<div class="chart-item-title" data-height="${item.height}">`;
-				c+= `${item.name}</div>`;
-			}
+                c+= `${item.name}</div>`;
+            }
 
-			let d = '</div>';
+            let d = '</div>';
 
             let bar = [a,b,c,d,d].join('');
 
             bars.push(bar);
 
-			// Axes
-			let e = `<div class="chart-x-axis-item">${item.x}</div>`;	
+            // Axes
+            let e = `<div class="chart-x-axis-item">${item.x ? item.x : ''}</div>`;	
 
-			axes.push({x:e});
+            axes.push({x:e});
         }
 
         return {bars,axes};
@@ -67,17 +67,17 @@ class Chart {
     // Injection for bar type graphs
     inject() {
         return function () {
-			let {bars, axes} = this.buildItems;
+            let {bars, axes} = this.buildItems;
 
             let con = document.getElementById(this.container),
                 html = '<div id="chart-max-width-container">';
 
             for (let bar in bars) html += bars[bar];
-			html += '</div></div>';
-			html += '<div class="chart-x-axis-container">';
-			for(let idx in axes) {
-				html += axes[idx].x;
-			}
+            html += '</div></div>';
+            html += '<div class="chart-x-axis-container">';
+            for(let idx in axes) {
+                html += axes[idx].x;
+            }
 
             con.innerHTML = html;
             con.id = 'chart-container';
@@ -123,12 +123,12 @@ class Chart {
                 let height = e.getAttribute('data-height');
                 e.style.lineHeight = `${height}px`;
             });
-			
-			let xAxisItems = document.querySelectorAll('.chart-x-axis-container .chart-x-axis-item');
-			xAxisItems.forEach(item => {
-				item.style.width = `${width}%`;
-				item.style.maxWidth = `${maxWidth}px`;
-			});
+
+            let xAxisItems = document.querySelectorAll('.chart-x-axis-container .chart-x-axis-item');
+            xAxisItems.forEach(item => {
+                item.style.width = `${width}%`;
+                item.style.maxWidth = `${maxWidth}px`;
+            });
         }
     }
 }
